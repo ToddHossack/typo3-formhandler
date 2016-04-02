57ce11d234a14f14e7ebe580e079bcae1deaec6f
Todd Hossack <todd@tiraki.com>
2016-04-03 09:55:06 +1200

    Added overriding render method to Tx_Formhandler_View_SubmittedOK

--- Classes/View/Tx_Formhandler_View_SubmittedOK.php
+++ Classes/View/Tx_Formhandler_View_SubmittedOK.php
@@ -55,5 +55,108 @@ class Tx_Formhandler_View_SubmittedOK extends Tx_Formhandler_View_Form {
 		$this->fillFileMarkers($markers);
 		$this->template = $this->cObj->substituteMarkerArray($this->template, $markers);
 	}
+	
+	public function render($gp, $errors) {
+
+		//set GET/POST parameters
+		$this->gp = $gp;
+
+		//set template
+		$this->template = $this->subparts['template'];
+		if(strlen($this->template) === 0) {
+			$this->utilityFuncs->throwException('no_template_file');
+		}
+
+		$this->errors = $errors;
+
+		//set language file
+		if (!$this->langFiles) {
+			$this->langFiles = $this->globals->getLangFiles();
+		}
+
+			//fill Typoscript markers
+		if (is_array($this->settings['markers.'])) {
+			$this->fillTypoScriptMarkers();
+		}
+		
+		//read master template
+		if (!$this->masterTemplates) {
+			$this->readMasterTemplates();
+		}
+
+		if (!empty($this->masterTemplates)) {
+			$count = 0;
+			while($count < 5 && preg_match('/###(field|master)_[^#]*###/', $this->template)) {
+				$this->replaceMarkersFromMaster();
+				$count++;
+			}
+		}
+		
+		if ($this->globals->getAjaxHandler()) {
+			$markers = array();
+			$this->globals->getAjaxHandler()->fillAjaxMarkers($markers);
+			$this->template = $this->cObj->substituteMarkerArray($this->template, $markers);
+		}
+		
+		//fill Typoscript markers
+		if (is_array($this->settings['markers.'])) {
+			$this->fillTypoScriptMarkers();
+		}
+
+		$this->substituteConditionalSubparts('has_translation');
+		if (!$this->gp['submitted']) {
+			$this->storeStartEndBlock();
+		} elseif (intval($this->globals->getSession()->get('currentStep')) !== 1) {
+			$this->fillStartEndBlock();
+		}
+		
+		if (intval($this->settings['fillValueMarkersBeforeLangMarkers']) === 1) {
+
+			//fill value_[fieldname] markers
+			$this->fillValueMarkers();
+		}
+
+		//fill LLL:[language_key] markers
+		$this->fillLangMarkers();
+
+		//substitute ISSET markers
+		$this->substituteConditionalSubparts('isset');
+		
+		//substitute IF markers
+		$this->substituteConditionalSubparts('if');
+
+		//fill default markers
+		$this->fillDefaultMarkers();
+
+		if (intval($this->settings['fillValueMarkersBeforeLangMarkers']) !== 1) {
+
+			//fill value_[fieldname] markers
+			$this->fillValueMarkers();
+		}
+
+		//fill selected_[fieldname]_value markers and checked_[fieldname]_value markers
+		$this->fillSelectedMarkers();
+		
+		//fill error_[fieldname] markers
+		if (!empty($errors)) {
+			$this->fillIsErrorMarkers($errors);
+			$this->fillErrorMarkers($errors);
+		}
+
+		//fill LLL:[language_key] markers again to make language markers in other markers possible
+		$this->fillLangMarkers();
+
+		//remove markers that were not substituted
+		$content = $this->utilityFuncs->removeUnfilledMarkers($this->template);
+		
+		if(is_array($this->settings['stdWrap.'])) {
+			$content = $this->cObj->stdWrap($content, $this->settings['stdWrap.']);
+		}
+		if(intval($this->settings['disableWrapInBaseClass']) !== 1) {
+			$content = $this->pi_wrapInBaseClass($content);
+		}
+
+		return $content;
+	}
 }
 ?>
